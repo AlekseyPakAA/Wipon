862a95017b458263f8e1015286a5b4874c0cc1db
aleksey_pak_aa <alexey.pak@umbrella-web.com>
2018-09-15 11:47:21 +0300

    Initial Commit

---
+++ Wipon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wipon
+//
+//  Created by Alexey Pak on 15/09/2018.
+//  Copyright © 2018 alexey.pak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
